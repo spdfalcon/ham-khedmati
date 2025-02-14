5017d03da10bae8683b171ab4c309269d2df4cff
spdfalcon <spd.mohammadreza@gmail.com>
2025-02-14 12:26:10 -0800

    debug 4

--- app/(auth)/reset-password/[token]/page.tsx
+++ app/(auth)/reset-password/[token]/page.tsx
@@ -1,9 +1,14 @@
-import AuthLayout from "@/app/_components/auth/AuthLayout";
-import ResetPasswordForm from "@/app/_components/auth/ResetPasswordForm";
+import { Metadata } from 'next';
+import AuthLayout from '@/app/_components/auth/AuthLayout';
+import ResetPasswordForm from '@/app/_components/auth/ResetPasswordForm';
 
-export default async function ResetPasswordPage({ params }: { params: { token: string } }) {
-  const resolvedParams = await params;
-  
+export const metadata: Metadata = {
+  title: 'تغییر رمز عبور | هم‌خدمتی',
+  description: 'تعیین رمز عبور جدید برای حساب کاربری',
+};
+
+export default async function ResetPasswordPage({ params }: { params: Promise<{ token: string }> }) {
+  const resolvedParams = await params; // حل کردن پرامیس
   return (
     <AuthLayout
       title="تعیین رمز عبور جدید"
