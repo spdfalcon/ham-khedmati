0746d49ff49e49746b6f5dbebc4544717f1f801a
spdfalcon <spd.mohammadreza@gmail.com>
2025-02-14 12:42:39 -0800

    debug 6

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // اختیاری: اگر می‌خواهید موقتاً ESLint رو غیرفعال کنید
+  },
+  typescript: {
+    ignoreBuildErrors: true, // اختیاری: اگر می‌خواهید موقتاً خطاهای TypeScript رو نادیده بگیرید
+  },
 };
 
 export default nextConfig;
